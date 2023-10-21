5d9fe281505986c14c13a8e1822c5419968fa9e6
Leonardo Silva <s2leoj@gmail.com>
2023-10-21 20:33:18 -0300

    Add variable/reference pattern

---
+++ Assets/Asteroid Game/Scripts/Variables/Reference/Reference.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+[Serializable]
+public class Reference { }
+
+/// <summary>
+/// Provide a flexible way of referencing values in Unity,
+/// allowing you to switch between constant values and variables.
+/// It also supports callbacks when the referenced value changes.
+/// </summary>
+[Serializable]
+public class Reference<T> : Reference
+{
+    public bool useConstant = true;
+    public T constantValue;
+    public Action<T> onValueChange;
+
+    public Variable variable;
+
+    public Reference() { }
+
+    public Reference(T value)
+    {
+        useConstant = true;
+        constantValue = value;
+    }
+
+    public virtual T Value
+    {
+        get { return useConstant ? constantValue : variable.GetValue<T>(); }
+    }
+
+    public static implicit operator T(Reference<T> reference)
+    {
+        return reference.Value;
+    }
+
+    public string GetName
+    {
+        get { return useConstant ? "Local Variable" : variable.name; }
+    }
+}
