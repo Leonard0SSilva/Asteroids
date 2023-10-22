3b9d7ecee84a484343c4c9413b522bd2600bc0ae
Leonardo Silva <s2leoj@gmail.com>
2023-10-22 19:28:19 -0300

    Add object pool

--- Assets/Asteroid Game/Scripts/General/ObjectPool.cs
+++ Assets/Asteroid Game/Scripts/General/ObjectPool.cs
@@ -15,37 +15,50 @@ public class ObjectPool : MonoBehaviour
     [Serializable]
     public class View
     {
-        public GameObjectListReference objectPool;
+        public List<GameObject> objectPool = new List<GameObject>();
     }
 
     public Settings settings;
-    public View view;
+    public View view = new();
 
-    private void Awake()
+    private void Start()
     {
         InitializePool();
     }
 
     private void InitializePool()
     {
-        view.objectPool.Set(new List<GameObject>());
+#if UNITY_EDITOR
+        Transform poolParent = new GameObject("Object Pool").transform;
+        poolParent.SetParent(gameObject.transform);
+#endif
 
+        view.objectPool = new List<GameObject>();
         for (int i = 0; i < settings.initialPoolSize; i++)
         {
             GameObject obj = Instantiate(settings.prefab);
             obj.AddComponent<Disposable>().onDestroy += RemoveObjectFromPool;
             obj.SetActive(false);
+#if UNITY_EDITOR
+            obj.transform.SetParent(poolParent, true);
+#endif
             view.objectPool.Add(obj);
         }
     }
 
+    public virtual void InitializeGameObject(GameObject obj)
+    {
+
+    }
+
     public GameObject GetObjectFromPool()
     {
-        foreach (GameObject obj in view.objectPool.Value)
+        foreach (GameObject obj in view.objectPool)
         {
             if (!obj.activeInHierarchy)
             {
                 obj.SetActive(true);
+                InitializeGameObject(obj);
                 return obj;
             }
         }
@@ -53,14 +66,25 @@ public class ObjectPool : MonoBehaviour
         // If all objects are in use, create a new one
         GameObject newObj = Instantiate(settings.prefab);
         view.objectPool.Add(newObj);
+        InitializeGameObject(newObj);
         return newObj;
+    }
+
+    public List<GameObject> GetObjectFromPool(int size)
+    {
+        var gameObjects = new List<GameObject>();
+        for (int i = 0; i < size; i++)
+        {
+            gameObjects.Add(GetObjectFromPool());
+        }
+        return gameObjects;
     }
 
     public void RemoveObjectFromPool(GameObject obj)
     {
-        if (!view.objectPool.Value.Contains(obj))
+        if (!view.objectPool.Contains(obj))
         {
-            view.objectPool.Value.Remove(obj);
+            view.objectPool.Remove(obj);
         }
     }
 }
