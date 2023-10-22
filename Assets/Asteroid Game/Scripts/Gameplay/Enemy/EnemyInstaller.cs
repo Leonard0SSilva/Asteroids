01604d1457ae65228f4bcbe06f6bf9903cb0a0f9
Leonardo Silva <s2leoj@gmail.com>
2023-10-22 19:31:01 -0300

    Setup asteroid

--- Assets/Asteroid Game/Scripts/Gameplay/Enemy/EnemyInstaller.cs
+++ Assets/Asteroid Game/Scripts/Gameplay/Enemy/EnemyInstaller.cs
@@ -5,12 +5,22 @@ public class EnemyInstaller : MonoBehaviour
     public int health;
     public Vector3Reference direction;
     public MovementController.Settings movetSettings;
+    public Vector2 minDirection, maxDirection;
+    public float minVelocity, maxVelocity;
 
     private void Awake()
     {
-        direction.Set(new Vector3(Mathf.Sign(Random.Range(-1, 1)), Mathf.Sign(Random.Range(-1, 1)), 0));
         movetSettings.direction = direction;
         gameObject.AddComponent<MovementController>().settings = movetSettings;
         gameObject.AddComponent<WrapAroundScreen>();
+    }
+
+    private void OnEnable()
+    {
+        float raondomVelocity = Random.Range(minVelocity, maxVelocity);
+        movetSettings.moveSpeed.Set(raondomVelocity);
+        movetSettings.maxSpeed.Set(raondomVelocity);
+        movetSettings.decelerationSpeed.Set(0);
+        direction.Set(new Vector3(Mathf.Sign(Random.Range(minDirection.x, maxDirection.x)), Mathf.Sign(Random.Range(minDirection.y, maxDirection.y)), 0));
     }
 }
