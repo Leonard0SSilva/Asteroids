b016f28f241d3985938a67b87880ee80f6fd747d
Leonardo Silva <s2leoj@gmail.com>
2023-10-23 05:04:43 -0300

    Add camera shake

---
+++ Assets/Asteroid Game/Scripts/Gameplay/General/CamerShake.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cam;
+    [SerializeField]
+    private float shakeDuration = 1.0f, shakeMagnitude = 0.7f, dampingSpeed = 1.0f;
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main.transform;
+        }
+        initialPosition = cam.localPosition;
+    }
+
+    public void ShakeCamera()
+    {
+        StartCoroutine(PerformShake());
+    }
+
+    private IEnumerator PerformShake()
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < shakeDuration)
+        {
+            Vector3 randomPoint = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            cam.localPosition = Vector3.Lerp(cam.localPosition, randomPoint, Time.deltaTime * dampingSpeed);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        cam.localPosition = initialPosition;
+    }
+}
